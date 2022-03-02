4c86f92f6d449a9c70c4013fffb8d4c148a4dec8
Angel Camacho <kmacho.an.34@gmail.com>
2022-03-01 20:37:33 -0600

    chore. change data url for post

--- src/app/core/enum/urlServices.enum.ts
+++ src/app/core/enum/urlServices.enum.ts
@@ -1,6 +1,6 @@
 export const enum UrlServices {
     userUrl = 'url to upsert user',//save user
-    dataUrl = 'https://sqhmz9rtyb.execute-api.us-east-1.amazonaws.com/dev/person/add',// save data
-    getDataUrl = ' https://qjm0ha3z6c.execute-api.us-east-1.amazonaws.com/dev/persons/list'//get data
+    dataUrl = 'https://qjm0ha3z6c.execute-api.us-east-1.amazonaws.com/dev/person/add',// save data
+    getDataUrl = 'https://qjm0ha3z6c.execute-api.us-east-1.amazonaws.com/dev/persons/list'//get data
 }
 
